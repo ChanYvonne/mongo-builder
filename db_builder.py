c1e889caff343066787bb3c9468912310ad8abb2
evookelj <evukelj@stuy.edu>
2017-02-03 13:51:39 -0500

    Congregating data works

--- db_builder.py
+++ db_builder.py
@@ -5,13 +5,33 @@ def setup():
 	server = MongoClient('127.0.0.1')
 	bonsai = server.bonsai
 	students = bonsai.students
-	peeps = DictReader(open("peeps.csv"))
-	courses = DictReader(open("courses.csv"))
-	print peeps
-	print courses
-	return [peeps,courses]
+	ps = DictReader(open("peeps.csv"))
+	courses = open("courses.csv")
+	return [ps, courses]
+
+def cong(peeps, crses):
+	ret = []
+	for peep in peeps:
+		this = {'name': peep['name'],
+				'age': peep['age'],
+				'id': peep['id'],
+				'courses': []
+		}
+		crses.seek(0)
+		courses = DictReader(crses)
+		for course in courses:
+			if course['id'] == peep['id']:
+				bayle = {}
+				bayle[course['code']] = course['mark']
+				this['courses'].append(bayle)
+		ret.append(this)
+	for thing in ret:
+		print thing
+		print ""
+	return ret
 
 def main():
 	arrs = setup()
-	print arrs[0]
-	print arrs[1]
+	tog = cong(arrs[0], arrs[1])
+
+main()
