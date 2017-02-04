64548bb4ba4e9ce9a3006ce4e487decf02efe801
evookelj <evukelj@stuy.edu>
2017-02-03 19:37:04 -0500

    Finish checking

--- db_builder.py
+++ db_builder.py
@@ -18,9 +18,7 @@ def cong(peeps, crses):
 		courses = DictReader(crses)
 		for course in courses:
 			if course['id'] == peep['id']:
-				bayle = {}
-				bayle[course['code']] = course['mark']
-				this['courses'].append(bayle)
+				this['courses'].append([course['code'], course['mark']])
 		ret.append(this)
 	return ret
 
@@ -28,19 +26,26 @@ def mongofy(info):
 	server = MongoClient('127.0.0.1')
 	bonsai = server.bonsai
 	students = bonsai.students
+        if students.count() > 0:
+                print "Db already exists. Clearing it and trying again..."
+                server.drop_database("bonsai")
+                mongofy(info)
+                return
 	for student in info:
 		students.insert_one(student)
 	print "Checking that everything was sent accurately! Printing contents\n"
-	for i in range(11):
-		print bonsai.students.find_one({'id': i-1})
-
-def check():
-	print ""
+	for doc in students.find():
+                print "Student: " + doc['name']
+                print "id: " + str(doc['id'])
+                print "age: " + str(doc['age'])
+                print "course grades:"
+                for course in doc['courses']:
+                        print course[0] + "grade: " + str(course[1])
+                print ""
 
 def main():
 	arrs = setup()
 	tog = cong(arrs[0], arrs[1])
 	serv = mongofy(tog)
-	check()
 
 main()
